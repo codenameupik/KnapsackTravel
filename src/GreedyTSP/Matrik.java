ad3f843754de0301f8e814c5fc4c005edc7e03d2
ERIC <ERIC@ERIC>
2016-10-15 17:38:28 +0700

    update 10x10

--- src/GreedyTSP/Matrik.java
+++ src/GreedyTSP/Matrik.java
@@ -1,13 +1,18 @@
 package GreedyTSP;
+
 public class Matrik {
-int graph[][]=new int[8][8];
-    Matrik(){
-            graph[1][1]=0;graph[1][2]=75;graph[1][3]=60;graph[1][4]=50;graph[1][5]=215;graph[1][6]=130;graph[1][7]=95;
-            graph[2][1]=60;graph[2][2]=0;graph[2][3]=60;graph[2][4]=70;graph[2][5]=85;graph[2][6]=50;graph[2][7]=135;
-            graph[3][1]=40;graph[3][2]=55;graph[3][3]=0;graph[3][4]=130;graph[3][5]=95;graph[3][6]=100;graph[3][7]=55;
-            graph[4][1]=40;graph[4][2]=75;graph[4][3]=140;graph[4][4]=0;graph[4][5]=45;graph[4][6]=60;graph[4][7]=135;
-            graph[5][1]=20;graph[5][2]=85;graph[5][3]=100;graph[5][4]=40;graph[5][5]=0;graph[5][6]=90;graph[5][7]=95;
-            graph[6][1]=120;graph[6][2]=55;graph[6][3]=110;graph[6][4]=60;graph[6][5]=95;graph[6][6]=0;graph[6][7]=65;
-            graph[7][1]=80;graph[7][2]=135;graph[7][3]=60;graph[7][4]=130;graph[7][5]=95;graph[7][6]=60;graph[7][7]=0;
-    }
+	int graph[][] = new int[11][11];
+
+	Matrik() {
+graph[1][1]=0;graph[1][2]=3;graph[1][3]=4;graph[1][4]=6;graph[1][5]=23;graph[1][6]=30;graph[1][7]=8;graph[1][8]=17;graph[1][9]=36;graph[1][10]=40;
+graph[2][1]=3;graph[2][2]=0;graph[2][3]=1;graph[2][4]=7;graph[2][5]=25;graph[2][6]=32;graph[2][7]=20;graph[2][8]=18;graph[2][9]=36;graph[2][10]=41;
+graph[3][1]=4;graph[3][2]=1;graph[3][3]=0;graph[3][4]=8;graph[3][5]=26;graph[3][6]=33;graph[3][7]=21;graph[3][8]=11;graph[3][9]=37;graph[3][10]=42;
+graph[4][1]=6;graph[4][2]=6;graph[4][3]=7;graph[4][4]=0;graph[4][5]=19;graph[4][6]=28;graph[4][7]=13;graph[4][8]=18;graph[4][9]=36;graph[4][10]=40;
+graph[5][1]=18;graph[5][2]=20;graph[5][3]=21;graph[5][4]=16;graph[5][5]=0;graph[5][6]=17;graph[5][7]=25;graph[5][8]=33;graph[5][9]=48;graph[5][10]=56;
+graph[6][1]=29;graph[6][2]=31;graph[6][3]=32;graph[6][4]=27;graph[6][5]=17;graph[6][6]=0;graph[6][7]=35;graph[6][8]=43;graph[6][9]=58;graph[6][10]=66;
+graph[7][1]=19;graph[7][2]=19;graph[7][3]=20;graph[7][4]=13;graph[7][5]=25;graph[7][6]=36;graph[7][7]=0;graph[7][8]=30;graph[7][9]=42;graph[7][10]=52;
+graph[8][1]=19;graph[8][2]=18;graph[8][3]=19;graph[8][4]=18;graph[8][5]=38;graph[8][6]=45;graph[8][7]=32;graph[8][8]=0;graph[8][9]=23;graph[8][10]=25;
+graph[9][1]=39;graph[9][2]=39;graph[9][3]=40;graph[9][4]=38;graph[9][5]=57;graph[9][6]=64;graph[9][7]=45;graph[9][8]=26;graph[9][9]=0;graph[9][10]=18;
+graph[10][1]=6;graph[10][2]=46;graph[10][3]=47;graph[10][4]=45;graph[10][5]=64;graph[10][6]=70;graph[10][7]=58;graph[10][8]=30;graph[10][9]=27;graph[10][10]=0;
+	}
 }
